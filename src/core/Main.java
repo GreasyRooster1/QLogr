302cbbafcf33a844ceaf9bf6a3dfe179d40115c5
QuantumDeity <greasyRooster1@gmail.com>
2024-05-18 13:24:56 -0700

    show debug

--- src/core/Main.java
+++ src/core/Main.java
@@ -5,6 +5,8 @@ import static core.QLogr.*;
 
 public class Main {
     public static void main(String[] args) {
+        setLogLevel(5);
+        debug("this is some debug message");
         info("this is some info");
         warn("oopsies, something went wrong");
         err("i have had an error!");
