ca339f1019445443d85647d44802d93c64ed9c4a
QuantumDeity <greasyRooster1@gmail.com>
2024-05-18 13:22:08 -0700

    formatting

--- src/core/LogHandler.java
+++ src/core/LogHandler.java
@@ -2,8 +2,13 @@ package core;
 
 public class LogHandler {
     protected static void logMessage(String message,LogType type,int logLevel){
-        if(type.getLevel()<=logLevel){
-            System.out.println(type.getLabel()+message);
+        if(type.getLevel()>logLevel) {
+            return;
         }
+        String formattedMessage = formatMessage(message,type);
+        System.out.println(formattedMessage);
+    }
+    private static String formatMessage(String message,LogType type){
+        return type.getLabel()+" | "+message;
     }
 }
