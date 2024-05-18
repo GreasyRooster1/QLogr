aab031a354c2d223b94421870cbc6fc9dac82da2
QuantumDeity <greasyRooster1@gmail.com>
2024-05-18 13:15:59 -0700

    made handle use type

---
+++ src/core/LogHandler.java
@@ -0,0 +1,9 @@
+package core;
+
+public class LogHandler {
+    protected static void logMessage(String message,LogType type,int logLevel){
+        if(type.getLevel()<logLevel){
+            System.out.println(type.getLabel()+message);
+        }
+    }
+}
