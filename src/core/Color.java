7e880290f467b9bffc98418d5999a2359185e1fc
QuantumDeity <greasyRooster1@gmail.com>
2024-05-18 20:01:42 -0700

    added bg colors

--- src/core/Color.java
+++ src/core/Color.java
@@ -20,7 +20,24 @@ public enum Color {
     BRIGHT_BLUE("\u001B[94m"),
     BRIGHT_MAGENTA("\u001B[95m"),
     BRIGHT_CYAN("\u001B[96m"),
-    BRIGHT_WHITE("\u001B[97m");
+    BRIGHT_WHITE("\u001B[97m"),
+
+    BG_BLACK("\u001B[40m"),
+    BG_RED("\u001B[41m"),
+    BG_GREEN("\u001B[42m"),
+    BG_YELLOW("\u001B[43m"),
+    BG_BLUE("\u001B[44m"),
+    BG_MAGENTA("\u001B[45m"),
+    BG_CYAN("\u001B[46m"),
+    BG_BG_WHITE("\u001B[47m"),
+    BG_BRIGHT_BLACK("\u001B[100m"),
+    BG_BRIGHT_RED("\u001B[101m"),
+    BG_BRIGHT_GREEN("\u001B[102m"),
+    BG_BRIGHT_YELLOW("\u001B[103m"),
+    BG_BRIGHT_BLUE("\u001B[104m"),
+    BG_BRIGHT_MAGENTA("\u001B[105m"),
+    BG_BRIGHT_CYAN("\u001B[106m"),
+    BG_BRIGHT_WHITE("\u001B[107m");
 
     private final String code;
 
